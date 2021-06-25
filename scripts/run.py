0df32eb16256c6893017d5e7cbfa5431c4396f9b
wutong8023 <wutong8023@gmail.com>
2021-06-26 02:52:19 +1000

    update

--- scripts/run.py
+++ scripts/run.py
@@ -80,16 +80,16 @@ def get_hyperlink(hyperlinks, mapping_name):
     
     # Todo 2: Change to your own link
     # Note: please check the branch name carefully!
-    str_hyperlink += "- Homepage [Overview](" + base_link + "README.md)\n"
+    str_hyperlink += "- Homepage [[Overview]](" + base_link + "README.md)\n"
     for i, item in enumerate(hyperlinks):
         str_hyperlink += "- " + mapping_name[item]
         all_link = "![](https://img.shields.io/badge/ALL-green)"
         nlp_link = "![](https://img.shields.io/badge/NLP-green)"
         cv_link = "![](https://img.shields.io/badge/CV-green)"
         
-        str_hyperlink += " of [All](" + base_link + "" + your_research_topic + "4all/" + item + ')'
-        str_hyperlink += " | [NLP](" + base_link + "" + your_research_topic + "4nlp/" + item + ')'
-        str_hyperlink += " | [CV](" + base_link + "" + your_research_topic + "4cv" + item + ')\n'
+        str_hyperlink += " of  [[All]](" + base_link + "" + your_research_topic + "4all/" + item + ')'
+        str_hyperlink += "  [[NLP]](" + base_link + "" + your_research_topic + "4nlp/" + item + ')'
+        str_hyperlink += "  [[CV]](" + base_link + "" + your_research_topic + "4cv" + item + ')\n'
     
     return str_hyperlink
 
