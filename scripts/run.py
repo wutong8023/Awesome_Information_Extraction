23d80d6f4a8d8603be486450b81058ef805fbaaa
wutong8023 <wutong8023@gmail.com>
2021-06-27 14:50:15 +1000

    update

--- scripts/run.py
+++ scripts/run.py
@@ -32,7 +32,7 @@ def plot_titles(titles):
 
 
 def get_outline(list_classif, count_list, filename, dicrib, add_hyperlink=False):
-    external_link = "[![](https://img.shields.io/badge/Awesome_Continual_Learning-yello)](https://github.com/wutong8023/Awesome_Continual_Learning.git) " \
+    external_link = "[![](https://img.shields.io/badge/Awesome_Continual_Learning-yellow)](https://github.com/wutong8023/Awesome_Continual_Learning.git) " \
                     "[![](https://img.shields.io/badge/Awesome_Few_Shot_learning-green)](https://github.com/wutong8023/Awesome_Few_Shot_Learning.git) " \
                     "[![](https://img.shields.io/badge/Awesome_Information_Extraction-blue)](https://github.com/wutong8023/Awesome_Information_Extraction.git) " \
                     "[![](https://img.shields.io/badge/Awesome_Ideas-red)](https://github.com/wutong8023/Awesome_Ideas.git)\n\n"
